3b3807fc1b4ddea3956012f2cdf6c8dc0414e3a3
Lucian (Luka) Lavric <151498964+lucianlavric@users.noreply.github.com>
2025-08-21 18:07:07 -0400

    Added error messages and backend validation to the name section

--- index.js
+++ index.js
@@ -64,25 +64,13 @@ const generateId = () => {
 	return String(maxId + 1)
 }
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
 	const body = request.body
 	console.log(body)
-
-	if (!body.name) {
-		return response.status(400).json({
-			error: 'name missing'
-		})
-	}
 
 	if (!body.number) {
 		return response.status(400).json({
 			error: 'number missing'
-		})
-	}
-
-	if (persons.find(person => person.name === body.name)) {
-		return response.status(400).json({
-			error: `${body.name} is already in the database`
 		})
 	}
 
@@ -97,6 +85,7 @@ app.post('/api/persons', (request, response) => {
 	person.save().then(savedPerson => {
 		response.json(savedPerson)
 	})
+		.catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -139,6 +128,8 @@ const errorHandler = (error, request, response, next) => {
 
 	if (error.name === 'CastError') {
 		return response.status(400).send({ error: 'malformatted id' })
+	} else if (error.name === 'ValidationError') {
+		return response.status(400).json({ error: error.message })
 	}
 
 	next(error)
